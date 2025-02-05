b849192af97511bbc81ab1ffe2c840361fa4f824
Mebimbi <129487346+Mebimbi@users.noreply.github.com>
2025-02-05 14:13:04 +0300

    Add files via upload

---
+++ WorkerService1Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using DataBaseConnection;
+using WorkerService1;
+namespace WorkerService1Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var entityObs = new Obss
+            {
+                DATE = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"),
+                TEMP_C = jsonEntity.Current.Temp_c,
+                CONDITION = jsonEntity.Current.Condition.Text,
+                NAME = jsonEntity?.Location.Name,
+                JSONRESPONSE = System.Text.Json.JsonSerializer.Serialize(jsonEntity)
+            };
+        }
+    }
+}
