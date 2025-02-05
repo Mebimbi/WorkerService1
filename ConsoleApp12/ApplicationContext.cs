1b219543c81b103372c57fb72a5e46f04c1c8bb9
Mebimbi <129487346+Mebimbi@users.noreply.github.com>
2025-02-05 14:14:52 +0300

    Add files via upload

---
+++ ConsoleApp12/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using DataBaseConnection;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+namespace DataBaseConnection;
+public class ApplicationContext : DbContext
+{
+    public DbSet<Obss> weatherobservations { get; set; } = null!;
+
+    public string connectionString;
+    public ApplicationContext(string connectionString)
+    {
+        this.connectionString = connectionString;
+        Database.EnsureCreated();
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(connectionString);
+    }   //"Host=localhost;Port=5432;Database=weatherobs;Username=postgres;Password=dkflfnjg"
+    //"Host=localhost;Port=5432;Database=weatherobs;Username=postgres;Password=dkflfnjg"
+}
---
+++ ConsoleApp12/Model/Obss.cs
@@ -0,0 +1,13 @@
+﻿namespace DataBaseConnection
+{
+    public class Obss
+    {
+        public int Id { get; set; }
+        public string? DATE { get; set; }
+        public double TEMP_C { get; set; }
+        public string? CONDITION { get; set; }
+        public string? NAME { get; set; }
+        public string? JSONRESPONSE { get; set; }
+        
+    }
+}
